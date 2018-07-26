a419ae2730b97ffa2f0aeb36e4a42945bd064e26
flippingflippers <citrussunshine2004@gmail.com>
2018-07-26 20:14:05 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -11,7 +11,7 @@ end
 end
 
 adele_goldberg = Person.new("adele_goldberg")
-# alan_kay = Person.new("alan_kay")
+alan_kay = Person.new("alan_kay")
 
-# puts adele_goldberg.get_name
-# puts alan_key.get_name
+puts adele_goldberg.get_name
+puts alan_kay.get_name
