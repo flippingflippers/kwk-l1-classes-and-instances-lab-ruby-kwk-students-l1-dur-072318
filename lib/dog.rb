fe5a8b87a97d6e98c2a1bf168ebaacfc36a0ec24
flippingflippers <citrussunshine2004@gmail.com>
2018-07-26 20:04:48 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,17 +3,17 @@ class Dog
   def initialize(name)
     @name = name
 end
-# def get_name
-#   return @name
-# end
+def get_name
+  return @name
+end
 end
   
 dog1 = Dog.new("Fido")
 dog2 = Dog.new("Snoopy")
 dog3 = Dog.new("Lassie")
 
-# puts dog1.get_name
-# puts dog2.get_name
-# puts dog3.get_name
+puts dog1.get_name
+puts dog2.get_name
+puts dog3.get_name
 
 
